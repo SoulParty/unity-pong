58f088335eceb52ff1d9a77c047a6ae93013f45f
Mindaugas K <dkibartas@exadel.com>
2015-11-19 00:43:26 +0200

    bug fixes

--- Assets/Scripts/MENU/PauseManager.cs
+++ Assets/Scripts/MENU/PauseManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class PauseManager : MonoBehaviour {
 
     public void returnToMainMenu() {
-        AdManager.Instance.hideBannerAd();
+//        AdManager.Instance.hideBannerAd();
+        GameController.Instance.pauseGame();
         Application.LoadLevel(0);
     }
 
